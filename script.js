7b6cd2255ff300b097020f133f8682c3ba34ed85
chiayewken <chiayewken@gmail.com>
2024-09-10 04:06:30 +0800

    Add wakelock for mobile devices

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let wakeLock = null;
+
 function getRandomRedImage() {
     const images = ["data/red_top_left.jpg", "data/red_top_right.jpg", "data/red_bottom_left.jpg", "data/red_bottom_right.jpg", "data/red_left.jpg", "data/red_right.jpg"];
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -15,4 +17,23 @@ function updateImages() {
     document.getElementById("image2").src = getRandomGreenImage();
 }
 
+async function requestWakeLock() {
+    try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        wakeLock.addEventListener('release', () => {
+            console.log('Screen Wake Lock released:', wakeLock.released);
+        });
+        console.log('Screen Wake Lock acquired:', wakeLock.released);
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
+}
+
+document.addEventListener('visibilitychange', async () => {
+    if (wakeLock !== null && document.visibilityState === 'visible') {
+        await requestWakeLock();
+    }
+});
+
+requestWakeLock();
 setInterval(updateImages, 2000);
